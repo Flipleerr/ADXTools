529a629884891869e3b69a175be044d01c4f8718
memoryleak <glupacsuper2009@gmail.com>
2024-06-25 23:05:37 +0200

    yes

---
+++ build/lib/adxtools/__init__.py
@@ -0,0 +1,8 @@
+from .adx_conversion import wav_to_adx, adx_to_wav
+from .ahx_conversion import wav_to_ahx, ahx_to_wav
+from .aix_conversion import adx_to_aix, aix_to_wav
+
+__version__ = '0.1.0'
+__author__ = 'memoryleak'
+__email__ = 'theflipleerr@gmail.com'
+__url__ = 'https://github.com/Flipleerr/ADXTools'
